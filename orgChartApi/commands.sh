3e46563de9a68b801fd2b4fe93bd991b15ca651f
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2025-07-07 09:55:42 +0530

    removed orgcharts

--- orgChartApi/commands.sh
+++
@@ -1,1 +0,0 @@
-cd ./build && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 ..
--- orgChartApi/main.cc
+++
@@ -1,9 +0,0 @@
-#include <drogon/drogon.h>
-int main() {
-    LOG_DEBUG << "Load config file";
-    drogon::app().loadConfigFile("../config.json");
-
-    LOG_DEBUG << "running on localhost:3000";
-    drogon::app().run();
-    return 0;
-}
